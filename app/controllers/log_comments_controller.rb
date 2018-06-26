0817c6d0a9b2ee7e44c3f02708be4385f7592442
Michal Wiacek <thepowerofra@gmail.com>
2018-06-26 20:06:19 +0200

    albo tak

--- app/controllers/log_comments_controller.rb
+++ app/controllers/log_comments_controller.rb
@@ -1,4 +1,6 @@
 class LogCommentsController < ApplicationController
+  before_action :correct_log_status
+
   def new
     @log = Log.find(params[:log_id])
     @log_comment = @log.log_comments.build
@@ -7,18 +9,23 @@ class LogCommentsController < ApplicationController
   def create
     @log = Log.find(params[:log_id])
     @log_comment = @log.log_comments.build(log_comment_params)
-    unless @log.closed?
       if @log_comment.save
-        redirect_to @log
+        redirect_to log_path(@log.id)
       else
         flash.now[:danger] = "error"
       end
-    end
   end
+
   private
+
     def log_comment_params
       params.require(:log_comment).permit(
         :comment,
         :log_id )
+    end
+
+    def correct_log_status
+      @log = Log.find(params[:log_id])
+      redirect_to root_url if @log.closed?
     end
 end
